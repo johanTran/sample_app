1ec5453656c0def5a1bc8b0d64f7194338558f1d
Johan Tran <rua.con.lon.ton.0107@gmail.com>
2015-12-08 05:56:55 -0800

    Finsih layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorials Sample App"
+    assert_select "title", "Ruby on Rails Tutorials Sample App"
   end
 
   test "should get help" do
@@ -17,6 +17,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorials Sample App"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorials Sample App"
   end
 
 end
